a5c7290920b9849a6aedde6804177fb148a4e605
yahelargas <yahelargas111@gmail.com>
2024-11-06 22:58:43 +0200

    add icons to job location

--- react-crash-course/src/components/JobListing.jsx
+++ react-crash-course/src/components/JobListing.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import {useState} from 'react'
+import { FaMapMarker} from 'react-icons/fa'
 
 const JobListing = ({ job }) => {
+  const [showFullDescripsion, setShowFullDescripsion] = useState(false)
+
+  let description = job.description
+
+  if (!showFullDescripsion){
+    description = description.substring(0, 90) + '...'
+  }
+
   return (
 
     <div className="bg-white rounded-xl shadow-md relative">
@@ -11,8 +20,14 @@ const JobListing = ({ job }) => {
                 </div>
 
                 <div className="mb-5">
-                    {job.description}
+                    {description}
                 </div>
+
+                <button onClick={()=>
+                  setShowFullDescripsion((PrevState) => !PrevState)} 
+                className='text-indigo-500 mb-5 hover:text-indigo-600'>
+                  {showFullDescripsion ? 'Less': 'More'}
+                </button>
 
                 <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
 
@@ -20,7 +35,7 @@ const JobListing = ({ job }) => {
 
                 <div className="flex flex-col lg:flex-row justify-between mb-4">
                   <div className="text-orange-700 mb-3">
-                    <i className="fa-solid fa-location-dot text-lg"></i>
+                    <FaMapMarker className='inline text-lg mb-1 mr-1'/>
                     {job.location}
                   </div>
                   <a
