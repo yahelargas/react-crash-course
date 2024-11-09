812ccc05c0398ef97384f0918decada0bdbfb4ce
yahelargas <yahelargas111@gmail.com>
2024-11-09 19:38:26 +0200

    Add + Delete job

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -5,19 +5,42 @@ import HomePage from './pages/HomePage'
 import AllJobsPage from './pages/AllJobsPage'
 import NotFoundPage from './pages/NotFoundPage'
 import JobPage, {jobLoader} from './pages/JobPage'
+import AddJobPage from './pages/AddJobPage'
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<MainLayout/>}>
-      <Route index element={<HomePage/>}/>
-      <Route path='/jobs' element={<AllJobsPage/>}/>
-      <Route path='/job/:id' element={<JobPage/>} loader={jobLoader}/>
-      <Route path='*' element={<NotFoundPage/>}/>
-    </Route>
-  )
-)
 
 const App = () => {
+
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newJob)
+    });
+    return;
+  }
+
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`,{
+      method: 'DELETE'
+    });
+    return;
+  }
+
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout/>}>
+        <Route index element={<HomePage/>}/>
+        <Route path='/jobs' element={<AllJobsPage/>}/>
+        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>}/>
+        <Route path='/job/:id' element={<JobPage deleteJob={deleteJob}/>} loader={jobLoader}/>
+        <Route path='*' element={<NotFoundPage/>}/>
+      </Route>
+    )
+  )
+
   return (
    <RouterProvider router={router}/>
   )
--- react-crash-course/src/pages/JobPage.jsx
+++ react-crash-course/src/pages/JobPage.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
-import { useParams , useLoaderData} from 'react-router-dom'
+import { useParams, useLoaderData, useNavigate} from 'react-router-dom'
 import { Link } from 'react-router-dom';
 import { FaArrowLeft , FaMapMarker} from 'react-icons/fa';
 
-const JobPage = () => {
+const JobPage = ({deleteJob}) => {
 
     const { id } = useParams();
     const job = useLoaderData();
+    const navigate = useNavigate();
+
+    const onDeleteClick = (jobId) => {
+        const confirm = window.confirm('Are you sure you want to delete this listing?')
+        if(!confirm){
+            return;
+        }
+
+        deleteJob(jobId);
+        navigate('/jobs');
+    }
 
   return (
     <>
@@ -85,7 +96,7 @@ const JobPage = () => {
                 className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >Edit Job</Link
               >
-              <button
+              <button onClick={ () => onDeleteClick(job.id)}
                 className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
               >
                 Delete Job
